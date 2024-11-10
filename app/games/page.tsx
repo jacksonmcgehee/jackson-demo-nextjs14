a0cdb0516c58ec27246aaf69d1c85307e269420e
Jackson McGehee <jacksonmcgehee@gmail.com>
2024-11-10 13:55:58 -0500

    Stubbed out basic app

---
+++ app/games/page.tsx
@@ -0,0 +1,12 @@
+
+
+const GamesPage = () => {
+    return (
+        <div>
+            <h1>Games</h1>
+            <p>This is the games page</p>
+        </div>
+    );
+};
+
+export default GamesPage;
